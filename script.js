9c877f6977b44ef043d18e6bdd8ddfb6cb9a7db2
Rebecca Lieberman <rebeccalieberman@gmail.com>
2016-11-13 13:26:00 -0500

    adding files

---
+++ script.js
@@ -0,0 +1,31 @@
+
+var questions = [
+  "How are you feeling right now?",
+  "Do you need to talk?",
+  "What just happened?",
+  "Do you need to talk about the election?",
+  "Do you have something to say?",
+  "Do you want to share how you're feeling?",
+  "Do you need a space to process?",
+  "Need to vent?",
+  "How do you feel about the election?",
+  "Do you have a story to share?",
+  "Want to talk about what just happened?",
+  "How do you feel?"
+]
+
+
+window.onload = function() {
+
+
+  var arr = questions.length;
+  var choice = Math.floor(Math.random() * arr);
+  console.log(choice);
+
+  var newQuestion = questions[choice];
+  console.log(newQuestion);
+
+  document.getElementById("question").innerHTML = newQuestion;
+
+  
+};
